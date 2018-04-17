53271f3cf1bd624c742061c18eec2cda0b951f1b
yeling <ycc360zaseycc360@sina.com>
2018-04-17 14:28:29 +0800

    读取文件内容

---
+++ src/readTest.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class readTest {
+    public static void main(String[] args) {
+
+        try {
+            FileReader fr = new FileReader("E:\\hello.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String str = "";
+            while ((str = br.readLine()) != null)
+                System.out.println(str);
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

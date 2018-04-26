9ae8f72d37f10862a99df46b89473ff879494252
madyeling <ycc360zaseycc360@sina.com>
2018-04-26 10:58:06 +0800

    读取目录

--- src/filer/xl1.java
+++ src/filer/xl1.java
@@ -1,17 +1,36 @@
 package filer;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class xl1 {
     public static void main(String[] args) {
+
         File file = new File("E:\\");
+
         if (file.isDirectory()) {
             String[] names = file.list();
+
             if (names != null) {
+
                 for (String name : names) {
                     System.out.println(name);
                 }
+
             }
+
         }
+
+        File file1 = new File("E:\\");
+        String[] filenameFilter = file1.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return new File(dir, name).isFile() && name.endsWith(".txt");
+            }
+        });
+        System.out.println("\n以下是后缀为.txt的文件：");
+        for (String name : filenameFilter)
+        System.out.println(name);
+
     }
 }

64a0e89af7be9ee0cc767f35b3062f8d60813f46
madyeling <ycc360zaseycc360@sina.com>
2018-05-03 10:45:20 +0800

    IO流练习题

---
+++ src/IOExam.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class IOExam {
+    public static void main(String[] args) {
+
+        try {
+            FileReader fr = new FileReader("C:\\Users\\admin\\Desktop\\abc.txt");
+            int a, b = 0, c = 0;
+            while ((a = fr.read()) != -1) {
+                if (a == 97)
+                    b++;
+                if (a == 65)
+                    c++;
+            }
+            fr.close();
+            System.out.println("字母a出现的次数为" + b);
+            System.out.println("字母a出现的次数为" + c);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Users\\admin\\Desktop\\abc.txt");
+            int a, b = 0, c = 0;
+
+            while ((a = fis.read()) != -1) {
+                char d = (char) a;
+                String e = d + "";
+
+                if (e.equals("a"))
+                    b++;
+                if (e.equals("A"))
+                    c++;
+
+            }
+
+            System.out.println("字母a出现的次数为" + b);
+            System.out.println("字母a出现的次数为" + c);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

2d4df1197d83edda4259cb506c75db04896345be
madyeling <ycc360zaseycc360@sina.com>
2018-05-17 11:23:51 +0800

    集合

--- src/set/array.java
+++ src/set/array.java
@@ -7,5 +7,12 @@ public class array {
         ArrayList al = new ArrayList();
         al.add("asd");
         System.out.println(al.get(0));
+
+        System.out.println("=============分割线=============");
+
+        ArrayList<String> as = new ArrayList<>();
+        as.add("赵鸡儿");
+        as.add("王尼玛");
+        System.out.println(as);
     }
 }
--- src/set/map.java
+++ src/set/map.java
@@ -1,7 +1,9 @@
 package set;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class map {
     public static void main(String[] args) {
@@ -9,7 +11,31 @@ public class map {
         m.put(1, "张三");
         m.put(2, "李四");
         m.put(3, "王五");
+        m.put(4, "张三");
+        m.put(5, "赵鸡儿");
 
-        System.out.println(m.get(1));
+//        for (int i = 1; i <= m.size(); i++)
+//            System.out.println(m.get(i));
+
+//        System.out.println(m);
+
+        Set s = m.keySet();
+        Iterator iterator = s.iterator();
+        while (iterator.hasNext()) {
+            Object Key = iterator.next();
+            Object value = m.get(Key);
+            System.out.println(Key + "\t" + value);
+        }
+
+        System.out.println("=============分割线=============");
+
+        Set es = m.entrySet();
+        Iterator i = es.iterator();
+        while (i.hasNext()) {
+            Map.Entry entry = (Map.Entry) (i.next());
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+
+        System.out.println("=============分割线=============");
     }
 }
